5602697e1624d07ee26dea39c9deb80ed667d38f
AlifIrawan <haikalalifrajairawan231@gmail.com>
2023-12-18 20:36:55 +0700

    modify : improving the code structure

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 ## Trigger the predict function from the model and get the API prediction endpoint :
 
 1. Clone the repository then open it using your code editor.
-2. Download the OCR model file with the __.pkl__ file format (or you can download it manually [here](https://drive.google.com/file/d/1oj9TXcMqGw8eSdkxZ4SoBWC-RLltfzV8/view?usp=sharing)) and name it "__model_COR.pkl__" (to match with the scripts), then create 2 new folders with the name "__OCR/model__" and move the OCR model to the "__model__" folder
-3. Now download the NER model you can download it [here](https://drive.google.com/file/d/1X9fk6UyP6BoBQZsGYMxqTcDrHeRx9Gxp/view?usp=sharing) and *extract here*, then create a new folder with the name "__NLP__" then move the folder you extracted earlier with the name "__model-best__" then now move it to the folder "__NLP__"
-4. This code uses Google Cloud Storage, so you will need to create your own GCS Bucket with the name "physude-apps", create a folder called "Image-questions" inside the bucket, take the credential file (a .json file) and name it "__physedu.json__" (to match with the script), then create a new folder called "__credential__" and copy the .json file to this folder.
-5. Open terminal in the project root directory, then run `pip install -r requirements.txt` to install the dependencies.
-6. Run the app using the command: `python main.py`.
-7. By default, the server will run on the localhost with the port 8000, open [http://localhost:8000](http://localhost:8000) to view it in your browser.
-8. If it shows 'OK' then you have successfully run the predict api.
-9. The next step is to configure the backend service, you can find it in the [backend](/) repository.
+2. Download the OCR model file with the __.pkl__ file format, you can download it [here](https://drive.google.com/file/d/1mPkD_LbiXgNRYdDyaSfc95oz2Piu6Inq/view?usp=sharing)
+3. Now download the NLP model you can download it [here](https://drive.google.com/file/d/1hqUd9mQQ-X3M5Y6zi0W_uozhSWNjnwwb/view?usp=sharing)
+4. After you download it now right click on all zip file and then click *extract here*
+5. This code uses Google Cloud Storage, so you will need to create your own GCS Bucket with the name "physude-apps", create a folder called "Image-questions" inside the bucket, take the credential file (a .json file) and name it "__physedu.json__" (to match with the script), then copy it to the root directory of this project.
+6. Open terminal in the project root directory.
+7. Run `pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu` and then run `pip install -r requirements.txt` to install the dependencies
+8. Run the app using the command: `python main.py`.
+9. By default, the server will run on the localhost with the port 8000, open [http://localhost:8000](http://localhost:8000) to view it in your browser.
+10. If it shows 'OK' then you have successfully run the predict api.
+11. The next step is to configure the backend service, you can find it in the [backend](/) repository.
--- main.py
+++ main.py
@@ -2,42 +2,43 @@ from flask import Flask, request, jsonify
 import pickle
 import os
 from google.cloud import storage
-
-# OCR Library
+from werkzeug.utils import secure_filename
 import cv2
-
-# NLP Library
 import spacy
 import re
 import os
 
 app = Flask(__name__)
-
-# Atur variabel lingkungan GOOGLE_APPLICATION_CREDENTIALS ke path file kredensial JSON Anda
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "physedu.json"
 
-# Path ke file JSON di Google Cloud Storage
+# Global Variabel
 bucket_name = "physude-apps"
 storage_client = storage.Client()
 bucket = storage_client.get_bucket(bucket_name)
+
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])  
+app.config['UPLOAD_FOLDER'] = './OCR/bucket-images/'
+app.config['OUTPUT_FOLDER'] = './OCR/output-images/'
+app.config['OCR_PATH'] = './OCR/model/model_COR.pkl'
+app.config['NLP_PATH'] = './NLP/model-best'
+
 ner = None
 result = None
 
-# Fungsi untuk download ke GCS
-def download_to_gcs(bucket_name, source_blob_name, destination_file_path, keyfile_path):
+# Check Image Format
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
-    # Create a client
+# Upload image to GCS
+def upload_from_gcs(bucket_name, local_file_path, remote_blob_name):
     client = storage.Client()
-
-    # Get the bucket and blob
     bucket = client.get_bucket(bucket_name)
-    blob = bucket.blob(source_blob_name)
+    blob = bucket.blob(remote_blob_name)
+    blob.upload_from_filename(local_file_path)
+    os.remove(local_file_path)
 
-    # Download the file from the bucket to the local file
-    blob.download_to_filename(destination_file_path)
-
-
-# Perhitungan fisika dari rumus
+# Formula calculation
 def calculate_result(entities_list, numbers_list):
     switch_dict = {
         ('massa', 'percepatan'): lambda: f'Besar gaya: {numbers_list[0] * numbers_list[1]}',
@@ -66,126 +67,124 @@ def calculate_result(entities_list, numbers_list):
         ('tegangan', 'arus listrik'): lambda: f'Besar resistansi/hambatan: {numbers_list[0] / numbers_list[1]}',
         # ... tambahkan kasus lain sesuai kebutuhan
     }
-
     key = tuple(entities_list)
-
     if key in switch_dict:
         return switch_dict[key]()
     else:
-        return print('Tidak ada aturan yang cocok untuk entities_list yang diberikan.')
+        return print('There is no matching rule for the given entities_list.')
 
-# Pre-processing gambar sebelum diload ke model
+# Resize image from client input
 def resize_image(input_path, output_path, target_size):
-    try:
-        image = cv2.imread(input_path)
+    image = cv2.imread(input_path)
+    scale_percent = target_size / max(image.shape[0], image.shape[1])
+    width = int(image.shape[1] * scale_percent)
+    height = int(image.shape[0] * scale_percent)
+    dim = (width, height)
+    resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    border_x = (target_size - resized_image.shape[1]) // 2
+    border_y = (target_size - resized_image.shape[0]) // 2
+    resized_image = cv2.copyMakeBorder(resized_image, border_y, border_y, border_x, border_x, cv2.BORDER_CONSTANT)
+    return cv2.imwrite(output_path, resized_image)
 
-        if image is None:
-            raise FileNotFoundError(f"file failed to upload, Make sure you upload a .PNG .JPG .JPEG file")
+# Load model OCR & Delete resized_image
+def load_model_OCR(ocr_path, image_model, output_path):
+    with open(ocr_path, 'rb') as model_file:
+        loaded_reader = pickle.load(model_file)
+    result = loaded_reader.readtext(image_model, detail=0)
+    os.remove(output_path)
+    return result
 
-        scale_percent = target_size / max(image.shape[0], image.shape[1])
-        width = int(image.shape[1] * scale_percent)
-        height = int(image.shape[0] * scale_percent)
-        dim = (width, height)
-
-        resized_image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-
-        border_x = (target_size - resized_image.shape[1]) // 2
-        border_y = (target_size - resized_image.shape[0]) // 2
-        resized_image = cv2.copyMakeBorder(resized_image, border_y, border_y, border_x, border_x, cv2.BORDER_CONSTANT)
-
-        # Save gambar yang sudah diresized
-        cv2.imwrite(output_path, resized_image)
-        respond = jsonify({'message': "Image successfully resized and saved"})
-        respond.status_code = 200
-        return respond
-
-    except Exception as e:
-        respond = jsonify({'message': str(e)})
-        respond.status_code = 400
-        return respond
-
-def image_to_model(output_path):
-    try:
-        # Path Gambar diconvert ke numpy array (dari opencv)
-        img_model = cv2.imread(output_path)
-    
-        if img_model is None:
-            raise FileNotFoundError(f"Image File Not Found")
-
-        return img_model
-
-    except FileNotFoundError as e:
-        respond = jsonify({'message': str(e)})
-        respond.status_code = 400
-        return respond
 
 
 @app.route('/', methods=['GET','POST'])
 def predict() :
     if request.method == 'POST' :
-
-        filename = request.json['filename']
-
-        download_to_gcs('physude-apps', 'Image-questions/'+filename, './OCR/bucket-images/'+filename, 'physedu.json')
-        
-        # Proses model OCR
-        ocr_path = './OCR/model/model_COR.pkl'
-        nlp_path = './NLP/model-best'
-        input_path = './OCR/bucket-images/'+filename
-        output_path = './OCR/output-images/resized_image.jpg'
+        reqImage = request.files['image']
+        ocr_path = app.config['OCR_PATH']
+        nlp_path = app.config['NLP_PATH']
         target_size = 1200
 
-        resize_image(input_path, output_path, target_size)
-        
-        image_to_model(output_path)
+        if reqImage and allowed_file(reqImage.filename):
 
-        with open(ocr_path, 'rb') as model_file:
-            loaded_reader = pickle.load(model_file)
-
-        # Hasil OCR model
-        result = loaded_reader.readtext(output_path, detail=0)
+            filename = secure_filename(reqImage.filename)
+            reqImage.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            input_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            output_path = os.path.join(app.config['OUTPUT_FOLDER'], filename)
             
-        ner = spacy.load(nlp_path)
+            resize_image(input_path, output_path, target_size)
 
-        array_string = ' '.join(map(str, result))
-        questions = ner(array_string)
+            image_model = cv2.imread(output_path)
 
-        variabel_list = []
-        numbers_list = []
+            result = load_model_OCR(ocr_path, image_model, output_path)
 
-        # Pattern regex
-        pattern = re.compile(r'\b(\d+)\s*(?=[a-zA-Z])|\b(\d+)\s*$')
+            ner = spacy.load(nlp_path)
 
-        # Looping variabel di soal
-        for ent in questions.ents:
-            variabel_list.append((ent.text))
-            variabel_list = variabel_list
+            array_string = ' '.join(map(str, result))
+            questions = ner(array_string)
 
-        # Looping nilai di soal
-        numbers_list = [match.group(1) or match.group(2) for match in pattern.finditer(array_string)]
+            variabel_list = []
+            numbers_list = []
 
-        # Bersihkan sebelum di predict
-        if len(variabel_list) > 2:
-            variabel_list.pop(2)
-        
-        numbers_list = [int(x) for x in numbers_list]
+            pattern = re.compile(r'\b(\d+)\s*(?=[a-zA-Z])|\b(\d+)\s*$')
 
-        result_calculate = calculate_result(variabel_list, numbers_list)
+            for ent in questions.ents:
+                variabel_list.append((ent.text))
+                variabel_list = variabel_list
 
-        # data convert ke JSON
-        # Periksa apakah array_string dan result_calculate tidak kosong
-        if array_string and result_calculate:
-            # Jika keduanya tidak kosong, buat data_to_save dengan nilai yang sesuai
-            data_to_save = {'Soal': array_string, 'Hasil_Perhitungan': result_calculate}
-        else:
-            # Jika salah satu atau keduanya kosong, buat data_to_save tanpa NULL
-            data_to_save = {'Soal': array_string or "Data Kosong", 'Hasil_Perhitungan': result_calculate or "Tidak ada perhitungan"}
+            numbers_list = [match.group(1) or match.group(2) for match in pattern.finditer(array_string)]
 
-        respond = jsonify(data_to_save)
-        respond.status_code = 200
-        return respond
+            if len(variabel_list) > 2:
+                variabel_list.pop(2)
+            
+            numbers_list = [int(x) for x in numbers_list]
 
-    return "OK"
+            result_calculate = calculate_result(variabel_list, numbers_list)
+
+            if array_string and result_calculate:
+                data_to_save = {'Soal': array_string, 'Hasil_Perhitungan': result_calculate}
+            else:
+                data_to_save = {'Soal': array_string or "Empty Data", 'Hasil_Perhitungan': result_calculate or "No calculation"}
+
+            upload_from_gcs(bucket_name, input_path, 'Image-questions/'+filename)
+
+            respond = jsonify(data_to_save)
+            respond.status_code = 200
+            return respond
+        else :
+            return jsonify({
+                'status': {
+                    'code': 400,
+                    'message': 'File failed to upload, Make sure you upload a .PNG .JPG .JPEG file'
+                }
+            }), 400
+
+    elif request.method == 'GET' :
+        return jsonify({
+        'Data': {
+            'Project': 'Capstone Bangkit 2023 Batch 2',
+            'Tema': 'Education, Learning, and Personal Development',
+            'Judul': 'Physedu - Smart Camera Calculator and Learning for Junior High School Physics Needs',
+            'Team': 'CH2-PS144',
+            'Anggota': [
+                { 'BangkitID': 'M006BSX1223', 'Nama': 'Ida Sri Afiqah', 'Universitas': 'Universitas Brawijaya' },
+                { 'BangkitID': 'M227BSY0228', 'Nama': 'Fasal Alif Haikal Irawan', 'Universitas': 'Universitas Jember' },
+                { 'BangkitID': 'M227BSX1101', 'Nama': 'Erika Divian Chandhani', 'Universitas': 'Universitas Jember' },
+                { 'BangkitID': 'C354BSY3093', 'Nama': 'Iko Raga Ahana Vidiantara', 'Universitas': 'Universitas Muhammadiyah Jember' },
+                { 'BangkitID': 'C408BSY4325', 'Nama': 'Slamet Rofikoh', 'Universitas': 'Sekolah Tinggi Ilmu Ekonomi Mandala' },
+                { 'BangkitID': 'A227BSY2674', 'Nama': 'Hendarta Widya Ardana', 'Universitas': 'Universitas Jember' },
+                { 'BangkitID': 'A622BSY2486', 'Nama': 'Rokman Nurhidayat', 'Universitas': 'Universitas Muhammadiyah Tangerang' },
+            ],
+            'Copyright': '©2023 All Rights Reserved!'
+        }
+    }), 200
+
+    else :
+        return jsonify({
+            'status': {
+                'code': 405,
+                'message': 'Method not allowed'
+            }
+        }), 405
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8000)
